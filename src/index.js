52df416410105730ee818212cc32a80224a878d1
Salem Ba-rabuod <barboud@gmail.com>
2026-02-20 00:03:59 +0100

    initialize a branch and tested a prompt input

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
-import { parseCommand } from "./command-parser.js";
+import { parseCommand } from './command-parser.js';
 
-// This is the entry point of your application. 
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
+// This is the entry point of your application.
 // Ask user for input, parse the command, and call the appropriate function from courseCommands.js or traineeCommands.js based on the command.
 
-console.log('Hello world');
+const userInput = prompt('Type anything! ');
+
+parseCommand(userInput);
